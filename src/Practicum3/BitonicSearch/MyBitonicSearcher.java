d128743af661ab1fad36f78bfe1a495f4ae77bcc
Flor De Meulemeester <flor.de.meulemeester@gmail.com>
2024-10-23 10:16:11 +0200

    Bitonic Search

--- src/Practicum3/BitonicSearch/MyBitonicSearcher.java
+++ src/Practicum3/BitonicSearch/MyBitonicSearcher.java
@@ -6,7 +6,28 @@ public class MyBitonicSearcher implements BitonicSearcher {
 
     @Override
     public int bitonicSearch(Sequence numbers, int query) {
-        return 0;
+        if (numbers.size() == 0) return -1;
+        return bitonicSearchRecursive(numbers, query, 0, numbers.size() - 1);
+    }
+
+    private int bitonicSearchRecursive(Sequence numbers, int query, int start, int end) {
+        if (start == end) {
+            if (numbers.get(start) == query) {
+                return start;
+            } else {
+                return -1;
+            }
+        }
+
+        int mid = (start + end) / 2;
+        int eersteDeel = bitonicSearchRecursive(numbers, query, start, mid);
+        int tweedeDeel = bitonicSearchRecursive(numbers, query, mid+1, end);
+
+        if (eersteDeel != -1) {
+            return eersteDeel;
+        } else {
+            return tweedeDeel;
+        }
     }
 
 }
